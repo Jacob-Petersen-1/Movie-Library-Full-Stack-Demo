741aaec7106489b42d30ce3720b4a8988a6c8be6
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-12-15 11:06:44 -0700

    Restyled NavBar Component

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,29 +2,68 @@ import React from "react";
 import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
+import {
+  Search,
+  Chat,
+  Notifications,
+  Person,
+  Image,
+  Home,
+} from "@material-ui/icons";
 import "./NavBar.css";
 
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <div className="navBar">
-      <ul>
-        <li className="brand">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Django JWT</b>
+    <div className="navbarContainer">
+      <div className="navbarLeft">
+        <span className="logo">Movie Review 🎬</span>
+      </div>
+      <div className="navbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input placeholder="Search" className="searchInput" />
+        </div>
+      </div>
+      <div className="navbarRight">
+        <div className="navbarLinks">
+          <Link to="/">
+            <Home />
           </Link>
-        </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
-        </li>
-      </ul>
+        </div>
+
+        <div className="navbarIcons">
+          <div className="navbarIconItem">
+            <Notifications />
+            <span className="navbarIconBadge">3</span>
+          </div>
+        </div>
+        {user ? (
+          <button onClick={logoutUser}>Logout</button>
+        ) : (
+          <button onClick={() => navigate("/login")}>Login</button>
+        )}
+      </div>
     </div>
   );
 };
 
 export default Navbar;
+
+// <div className="navBar">
+//   <ul>
+//     <li className="brand">
+//       <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+//         <b>React/Django JWT</b>
+//       </Link>
+//     </li>
+//     <li>
+//       {user ? (
+//         <button onClick={logoutUser}>Logout</button>
+//       ) : (
+//         <button onClick={() => navigate("/login")}>Login</button>
+//       )}
+//     </li>
+//   </ul>
+// </div>
